5e7d8cf7b0c04322c0095b95fb272b57ac703f54
hunterchaves <emanuelchaves199@gmaol.com>
2025-09-29 23:24:19 -0300

    Revert "feat: Implement localStorage caching for dishes"
    This reverts commit a3ce7f68252bc706a279179aa78170c18e9ab68f.

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Dish } from '../models';
 
 @Injectable({
@@ -9,21 +8,11 @@ import { Dish } from '../models';
 })
 export class DishService {
   private apiUrl = 'http://localhost:8080/api/dishes';
-  private readonly dishesCacheKey = 'dishes';
 
   constructor(private http: HttpClient) { }
 
   getDishes(): Observable<Dish[]> {
-    const cachedDishes = localStorage.getItem(this.dishesCacheKey);
-    if (cachedDishes) {
-      return of(JSON.parse(cachedDishes));
-    }
-
-    return this.http.get<Dish[]>(this.apiUrl).pipe(
-      tap(dishes => {
-        localStorage.setItem(this.dishesCacheKey, JSON.stringify(dishes));
-      })
-    );
+    return this.http.get<Dish[]>(this.apiUrl);
   }
 
   getDish(id: number): Observable<Dish> {
@@ -31,26 +20,14 @@ export class DishService {
   }
 
   createDish(dish: Omit<Dish, 'id'>): Observable<Dish> {
-    return this.http.post<Dish>(this.apiUrl, dish).pipe(
-      tap(() => {
-        localStorage.removeItem(this.dishesCacheKey);
-      })
-    );
+    return this.http.post<Dish>(this.apiUrl, dish);
   }
 
   updateDish(id: number, dish: Dish): Observable<Dish> {
-    return this.http.put<Dish>(`${this.apiUrl}/${id}`, dish).pipe(
-      tap(() => {
-        localStorage.removeItem(this.dishesCacheKey);
-      })
-    );
+    return this.http.put<Dish>(`${this.apiUrl}/${id}`, dish);
   }
 
   deleteDish(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
-      tap(() => {
-        localStorage.removeItem(this.dishesCacheKey);
-      })
-    );
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
